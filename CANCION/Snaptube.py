bf9ade6f0e1a4b5e254ee1fded6d3433a6eff1fa
Samuel Villa <polessamu@gmail.com>
2026-08-27 13:01:19 -0500

    Inicializacion de mi proyecto

---
+++ CANCION/Snaptube.py
@@ -0,0 +1,81 @@
+class Snaptube:
+    def __init__(self, titulo, artista, año, genero):
+        self.titulo = titulo
+        self.artista = artista
+        self.año = año
+        self.genero = genero
+        self.siguiente = None
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, titulo, artista, año, genero): # En el menu hay que hacer que lo puedan insertar por cada atributo
+        new_song = Snaptube(titulo, artista, año, genero)
+
+        if self.head is None:
+            self.head = new_song
+        else:
+            actual = self.head
+
+            while actual.siguiente is not None:
+                actual = actual.siguiente
+
+            actual.siguiente = new_song
+
+    def buscar(self, titulo): # Solamente que recorra la lista y ya se muestra el resultado
+        actual = self.head
+
+        while actual is not None:
+            if actual.titulo.lower() == titulo.lower():
+                print("Canción encontrada:")
+                print(f"Título: {actual.titulo}")
+                print(f"Artista: {actual.artista}")
+                print(f"Año: {actual.año}")
+                print(f"Género: {actual.genero}")
+                return actual
+
+            actual = actual.siguiente
+
+        print("Canción no encontrada.")
+        return None
+
+    def show_list(self):
+        current = self.head
+        while current is not None:
+            print(f"Titulo: {current.data[0]}\n Artista: {current.data[1]}\n Año: {current.data[2]}\n Genero: {current.data[3]}")
+    
+list = Linked_list()
+
+while True:
+    print("SNAPTUBE")
+    print("1. Insertar cancion")
+    print("2. Buscar cancion")
+    print("3. Mostrar canciones")
+    print("4. Salir")
+
+
+    option = input("Selccione una opcion: ")
+
+    if option == "1":
+        print("INSERTAR CANCION:")
+        titulo = input("Titulo: ")
+        artista = input("Artista: ")
+        año = input("Año: ")
+        genero = input("Genero: ")
+
+        list.insert(
+            titulo,
+            artista,
+            año,
+            genero
+        )
+    elif option == "2":
+        print("Buscar cancion")
+        list.buscar(titulo)
+    elif option == "3":
+        print("Mostrar canciones: ")
+        list.show_list()
+    else:
+        print("--SALIENDO DEL SISTEMA...--")
+        break
---
+++ LINKED LIST/nodo.py
@@ -0,0 +1,99 @@
+"""class Nodo:
+    def __init__(self, dato):
+        self.dato = dato
+        self.siguiente = None
+
+Nodo1 = Nodo(42)
+print(Nodo1.dato)
+print(Nodo1.siguiente)
+
+Nodo2 = Nodo(67)
+print(Nodo2.dato) 
+print(Nodo2.siguiente)
+
+
+Nodo3 = Nodo(10)
+print(Nodo3.dato) 
+print(Nodo3.siguiente)
+
+Nodo1.siguiente = Nodo2
+Nodo2.siguiente = Nodo3
+
+actual = Nodo1
+contador = 1
+
+while actual is not None:
+    print(f"Nodo {contador} Dato {actual.dato} Direccion {id(actual)}")
+    actual = actual.siguiente
+    contador += 1"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def show(self):
+        print(f"Data = {self.data}")      
+        print(f"Next = {self.next}") 
+
+
+class Linked_list:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.size = 0
+
+    def insert_first(self, data):
+        new_node = Node(data)
+        if (self.head == None):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.size += 1
+
+    def insert_last(self,data):
+        new_node = Node(data)
+        if (self.head == None):
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.size += 1
+
+    def insert_at(self, data, position):
+        if (position == 0):
+            self.insert_first(data)
+        elif (position == self.size-1):
+            self.insert_last(data)
+        elif (position > self.size):
+            print("ERROR...." \
+            "The data can´t be inserted")
+        else:
+            previous = self.head
+            k=0
+            while k < position-1:
+                previous = previous.next
+                new_node = Node(data)
+                new_node.next = previous.next
+                previous.next = new_node
+            self.size += 1 
+
+
+    def show_list(self):
+        print(f"Head = {self.head} --- Tail = {self.tail} --- Size = {self.size}")
+        print("Nodes: ")
+        current = self.head
+        while current is not None:
+            print(f"data = {current.data} ---> next = {current.next}")
+            current = current.next
+
+new_list = Linked_list()
+new_list.insert_first(43)
+new_list.insert_last(10)
+print(f"Tamaño de la lista: {new_list.size}")
+new_list.insert_at(5, 2)
+new_list.insert_at(3, 6)
+new_list.show_list()

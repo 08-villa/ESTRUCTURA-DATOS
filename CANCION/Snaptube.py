e87d8de29ec79d571101f58e543237e27cf5f42d
Samuel Villa <polessamu@gmail.com>
2026-08-28 06:55:54 -0500

    cambio en el titulo

--- CANCION/Snaptube.py
+++ CANCION/Snaptube.py
@@ -48,7 +48,7 @@ class Linked_list:
 list = Linked_list()
 
 while True:
-    print("APP ---> SNAPTUBE")
+    print("SPOTIFY")
     print("1. Insertar cancion")
     print("2. Buscar cancion")
     print("3. Mostrar canciones")

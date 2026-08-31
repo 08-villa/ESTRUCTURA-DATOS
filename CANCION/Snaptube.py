5ae425a9304a022a2a284cac33e2777ebc2f4b33
Samuel Villa <polessamu@gmail.com>
2026-08-31 07:39:22 -0500

    Metodo eliminar

--- CANCION/Snaptube.py
+++ CANCION/Snaptube.py
@@ -23,22 +23,25 @@ class Linked_list:
 
             actual.siguiente = new_song
 
-    def buscar(self, titulo): # Solamente que recorra la lista y ya se muestra el resultado
+    #def buscar(self, titulo):
+
+    def eliminar(self):
+
+        titulo = input("Ingrese el nombre de la cancion que desea eliminar: ")
         actual = self.head
+        anterior = None
 
         while actual is not None:
             if actual.titulo.lower() == titulo.lower():
-                print("Canción encontrada:")
-                print(f"Título: {actual.titulo}")
-                print(f"Artista: {actual.artista}")
-                print(f"Año: {actual.año}")
-                print(f"Género: {actual.genero}")
-                return actual
-
+                if anterior is None:
+                    self.head = actual.siguiente
+                else:
+                    anterior.siguiente = actual.siguiente
+                print(f"La cancion {actual.titulo} fue eliminada.")
+                return
+            anterior = actual
             actual = actual.siguiente
-
-        print("Canción no encontrada.")
-        return None
+        print("Cancion no encontrada.")
 
     def show_list(self):
         current = self.head
@@ -52,10 +55,11 @@ while True:
     print("1. Insertar cancion")
     print("2. Buscar cancion")
     print("3. Mostrar canciones")
-    print("4. Salir")
+    print("4. Eliminar canciones")
+    print("5. Salir")
 
 
-    option = input("Selccione una opcion: ")
+    option = input("Seleccione una opcion: ")
 
     if option == "1":
         print("INSERTAR CANCION:")
@@ -71,12 +75,16 @@ while True:
             genero
         )
     elif option == "2":
-        print("Buscar cancion")
-        list.buscar(titulo)
+        print(input("Buscar cancion: "))
+        list.buscar()
     elif option == "3":
         print("Mostrar canciones: ")
-        list.show_list()
     elif option == "4":
+        print("Eliminar Canciones")
+        list.eliminar()
+
+        list.show_list()
+    elif option == "5":
         print("Saliendo del sistema...")
         break
     else:
